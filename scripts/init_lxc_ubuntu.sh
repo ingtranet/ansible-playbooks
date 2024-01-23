603cc80eabb9ceb64a58d0a9ff1df61788edf349
cookieshake <cookieshake.dev@gmail.com>
2024-01-24 01:08:13 +0900

    install gpu node

--- scripts/init_lxc_ubuntu.sh
+++ scripts/init_lxc_ubuntu.sh
@@ -4,6 +4,6 @@ apt update -y
 apt upgrade -y
 apt install -y openssh-server vim
 echo "PermitRootLogin yes" > /etc/ssh/sshd_config.d/permit-root-login.conf
-systemctl enable sshd
-systemctl start sshd
+systemctl enable ssh
+systemctl start ssh
 echo 'L /dev/kmsg - - - - /dev/console' > /etc/tmpfiles.d/kmsg.conf
